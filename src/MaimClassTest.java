3cecac3b0cfcc1ed0d7fdb40fc0c4194a25c5ee6
RudAlexOd <48775228+RudAlexOd@users.noreply.github.com>
2021-04-06 18:47:24 +0300

    testThird

--- src/MaimClassTest.java
+++ src/MaimClassTest.java
@@ -3,16 +3,33 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MaimClassTest extends MainClass
-{
+import java.util.HashMap;
+
+public class MaimClassTest extends MainClass {
     @Test
-    public void testGetLocalNumber()
-    {
-        Assert.assertTrue("this.getLocalNumber() != 14",this.getLocalNumber()==14);
+    public void testGetLocalNumber() {
+        Assert.assertTrue("this.getLocalNumber() != 14", this.getLocalNumber() == 14);
     }
+
     @Test
-    public void testGetClassNumber()
+    public void testGetClassNumber() {
+
+        if (this.class_number == 45) {
+            Assert.assertTrue("this.getLocalNumber() = 45", this.getLocalNumber() == 45);
+        } else {
+            Assert.assertTrue("this.getClassNumber () < 45", this.getClassNumber() > 45);
+        }
+    }
+
+    @Test
+    public void testGetClassString()
     {
-        Assert.assertTrue("this.getClassNumber () < 45",this.getClassNumber() > 45);
+       boolean newStr;
+       newStr = this.class_string.contains("hello");
+
+        {
+            Assert.assertTrue("In this.getClass String() without substring _hello and _Hello", newStr!=this.class_string.contains("Hello"));
+        }
+
     }
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -5,11 +5,19 @@ class MainClass
     {
         return 14;
     }
-       int class_number = 20;
+       int class_number = 30;
 
     public int getClassNumber()
     {
        return this.class_number;
     }
+
+    String class_string = "Hello, world";
+
+    public String getClassString()
+    {
+        return this.class_string;
+    }
+
 
 }
